ee2fc37880ba71ec35d6a925714a579852303305
Кириллов Иван <vanyusha_kirillov_03@mail.ru>
2026-05-17 12:38:04 +0300

    Отключение js и создание шапки сайта + первой секции main

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,19 +1,19 @@
 /* в этот файл добавляет скрипты*/
 
-let navMain = document.querySelector('.header');
-let navToggle = document.querySelector('.header__menu-button');
+// let navMain = document.querySelector('.header');
+// let navToggle = document.querySelector('.header__menu-button');
 
-navMain.classList.remove('header--nojs');
+// navMain.classList.remove('header--nojs');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('header--closed')) {
-    navMain.classList.remove('header--closed')
-    navMain.classList.add('header--opened');
-  } else {
-    navMain.classList.add('header--closed');
-    navMain.classList.remove('header--opened');
-  }
-});
+// navToggle.addEventListener('click', function () {
+//   if (navMain.classList.contains('header--closed')) {
+//     navMain.classList.remove('header--closed')
+//     navMain.classList.add('header--opened');
+//   } else {
+//     navMain.classList.add('header--closed');
+//     navMain.classList.remove('header--opened');
+//   }
+// });
 // Переполнение
 /*
 function getTextNodesIn(elem, opt_fnFilter) {
